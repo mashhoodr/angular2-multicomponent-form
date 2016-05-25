80c16a33aea3696f05d590d43755e9c848fd75d1
Narain Sagar <narainmenghwar@gmail.com>
2016-05-26 03:08:29 +0500

    sidebar component component added

---
+++ src/app/sidebar/sidebar.component.spec.ts
@@ -0,0 +1,50 @@
+import {
+  beforeEach,
+  beforeEachProviders,
+  describe,
+  expect,
+  it,
+  inject,
+} from '@angular/core/testing';
+import { ComponentFixture, TestComponentBuilder } from '@angular/compiler/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { SidebarComponent } from './sidebar.component';
+
+describe('Component: Sidebar', () => {
+  let builder: TestComponentBuilder;
+
+  beforeEachProviders(() => [SidebarComponent]);
+  beforeEach(inject([TestComponentBuilder], function (tcb: TestComponentBuilder) {
+    builder = tcb;
+  }));
+
+  it('should inject the component', inject([SidebarComponent],
+      (component: SidebarComponent) => {
+    expect(component).toBeTruthy();
+  }));
+
+  it('should create the component', inject([], () => {
+    return builder.createAsync(SidebarComponentTestController)
+      .then((fixture: ComponentFixture<any>) => {
+        let query = fixture.debugElement.query(By.directive(SidebarComponent));
+        expect(query).toBeTruthy();
+        expect(query.componentInstance).toBeTruthy();
+      });
+  }));
+
+  it('should have as title \'Sidebar\'',
+      inject([SidebarComponent], (sidebar: SidebarComponent) => {
+    expect(sidebar.title).toEqual('Sidebar');
+  }));
+});
+
+@Component({
+  selector: 'test',
+  template: `
+    <app-sidebar></app-sidebar>
+  `,
+  directives: [SidebarComponent]
+})
+class SidebarComponentTestController {}
+
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,30 @@
+import { ControlGroup } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormManager } from '../shared/form-manager';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-sidebar',
+  templateUrl: 'sidebar.component.html',
+  styleUrls: ['sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  title: string = "Sidebar";
+  mainForm: ControlGroup;
+  steps: Object = {}; /* contains all steps validations (true/false) */
+  
+  constructor(public fm: FormManager) {
+    this.mainForm = fm.mainForm;
+
+    // initialize all `steps` to `false` by default.
+    for (let key in this.mainForm.value) {
+      this.steps[key] = false;
+    }
+  }
+
+  getSteps(): Array<string>{
+    return Object.keys(this.steps);
+  }
+  
+  ngOnInit() {}
+}
