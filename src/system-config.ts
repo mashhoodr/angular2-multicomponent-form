6e70dc227f8256a80cfcd8fa9e9520950d8dc44a
Mashhood Rastgar <mashhoodr@gmail.com>
2016-06-03 17:05:52 +0500

    removed radio button refs

--- src/system-config.ts
+++ src/system-config.ts
@@ -31,8 +31,7 @@ const barrels: string[] = [
   'app/shared',
   'app/+step1',
   'app/+step2',
-  'app/shared/form-field',
-  'app/shared/radio-button',
+  'app/shared/form-field'
   /** @cli-barrel */
 ];
 
