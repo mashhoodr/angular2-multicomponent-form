8b826dec44673452443c07d22b624f6486e35c28
ZainabKhan <zainab@marketlytics.com>
2016-06-10 12:45:21 +0500

    tests added for editable directive

--- src/app/shared/form-field/editable.directive.spec.ts
+++ src/app/shared/form-field/editable.directive.spec.ts
@@ -8,22 +8,56 @@ import {
   it,
   inject
 } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import {ComponentFixture, TestComponentBuilder} from '@angular/compiler/testing';
 import { provide, Component } from '@angular/core';
 import { EditableDirective } from './editable.directive';
 
 @Component({
   selector: 'test-component',
-  template: `<div editable></div>`
+  template: `<input type="text" editable />`,
+   directives: [EditableDirective],
 })
 class TestComponent {}
 
-describe('Editable Directive', () => {
+describe('Editable Directive: ', () => {
   beforeEachProviders((): any[] => []);
 
-  it('should ...', async(inject([TestComponentBuilder], (tcb:TestComponentBuilder) => {
-    return tcb.createAsync(TestComponent).then((fixture: ComponentFixture<any>) => {
-      fixture.detectChanges();
-    });
-  })));
+  it('should add attribute', inject([TestComponentBuilder], (testComponentBuilder: TestComponentBuilder) => {
+    return testComponentBuilder
+      .createAsync(TestComponent)
+      .then((fixture: ComponentFixture<TestComponent>) => {
+        fixture.detectChanges();
+        const directiveEl = fixture.debugElement.query(By.css('[editable]'));
+        expect(directiveEl.nativeElement).toBeDefined();
+      });
+  }));
+
+  it('input value should be emptied on focus', inject([TestComponentBuilder], (testComponentBuilder: TestComponentBuilder) => {
+    return testComponentBuilder
+      .createAsync(TestComponent)
+      .then((fixture: ComponentFixture<TestComponent>) => {
+        fixture.detectChanges();
+        const directiveEl = fixture.debugElement.query(By.css('[editable]'));
+        directiveEl.nativeElement.value = 'testing';
+        directiveEl.nativeElement.focus();
+
+        expect(directiveEl.nativeElement.value).toEqual('');
+      });
+  }));
+
+  it('input value should be restored on blur', inject([TestComponentBuilder], (testComponentBuilder: TestComponentBuilder) => {
+    return testComponentBuilder
+      .createAsync(TestComponent)
+      .then((fixture: ComponentFixture<TestComponent>) => {
+        fixture.detectChanges();
+        const directiveEl = fixture.debugElement.query(By.css('[editable]'));
+
+        directiveEl.nativeElement.value = 'testing';
+        directiveEl.nativeElement.focus();
+        directiveEl.nativeElement.blur();
+
+        expect(directiveEl.nativeElement.value).toEqual('testing');
+      });
+  })); 
 });
