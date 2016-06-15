36c0483519fda9e2b515b74341aa2d2b04a7947d
Narain Sagar <narainmenghwar@gmail.com>
2016-06-15 13:45:15 +0500

    added support for tooltips

---
+++ src/app/shared/form-field/tooltip.ts
@@ -0,0 +1,22 @@
+export class Tooltip {
+	/*
+	Params:
+	- text: text of tooltip
+	- placement: tooltip positioning instruction, supported positions: 'top', 'bottom', 'left', 'right'
+	- animation: if `false` fade tooltip animation will be disabled
+	- enable: if `false` tooltip is disabled and will not be shown
+	- isOpen: if `true` tooltip is currently visible
+	*/
+
+	constructor(
+		public text: string = '',
+		public placement: string = 'top',
+		public animation: boolean = true,
+		public enable: boolean = true,
+		public isOpen: boolean = false
+	) {
+		if(!this.text) {
+			this.enable = false;
+		}
+	}
+}
