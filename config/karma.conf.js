a3b09ffe378e9d0c218336423f6a98b459af1d2a
Mashhood Rastgar <mashhoodr@gmail.com>
2016-06-27 11:24:53 +0500

    switched to phantomjs for unit testing

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
     frameworks: ['jasmine'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher')
+      require('karma-phantomjs-launcher')
     ],
     customLaunchers: {
       // chrome setup for travis CI using chromium
@@ -36,7 +36,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
     singleRun: false
   });
 };
